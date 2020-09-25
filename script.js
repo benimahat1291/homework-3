ce7e43279d65c5330d3d069972a9df6c71bcaf3b
Beni Mahat <benimahat6565@gmail.com>
2020-09-25 17:38:57 -0600

    frist commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Assignment Code, refrence HTML IDS
+var generateBtn = document.getElementById('generate');
+var numOfCharBtn = document.getElementById('numOfCharsBox');
+var upperboxcheck = document.getElementById('upperBoxId');
+var numberboxcheck = document.getElementById('numbersBoxId');
+var symbolboxcheck = document.getElementById('symbolBoxId');
+var passworddisplay = document.getElementById('password');
+
+
+// Define Arrays used for Symbol from char codes
+const uppercharcodes= charArray(65,90);
+const lowercharcodes= charArray(97,122);
+const numbercharcodes = charArray(48, 57);
+const symbolcharcodes = charArray(48, 57);
+
+// When sclicking on the submit button hold the information
+generateBtn.addEventListener('click',() =>{
+  var length = numOfCharBtn.value;
+  var upper = upperboxcheck.checked;
+  var number = numberboxcheck.checked;
+  var symbol = symbolboxcheck.checked;
+
+  passworddisplay.innerText = generatePassword(length,upper,number,symbol);
+});
+
+function generatePassword(length,upper,number,symbol){
+  let charCodes = lowercharcodes;
+  if (upper) charCodes = charCodes.concat(uppercharcodes);
+  if (number) charCodes = charCodes.concat(numbercharcodes);
+  if (symbol) charCodes = charCodes.concat(symbolcharcodes);
+
+  
+  const passwordChars = [];
+  for (let i = 0; i < length; i++){ // index starting at zero, loop untill max characters met
+      const arraynumber = charCodes[Math.floor(Math.random()* charCodes.length)]; // assigns random num from array
+      passwordChars.push(String.fromCharCode(arraynumber)); //calling on Charcodesheet given num values from array
+
+  };
+  const password = passwordChars.join(''); //turns list of strings into a single string
+  return password;
+
+
+};
+
+function charArray(min,max){
+  const array = [];
+  for (let i = min; i<= max; i++) { // fill array with the given range
+      array.push(i);
+  };
+  return array;
+};
+
