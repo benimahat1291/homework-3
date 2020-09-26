3c87c4d3072781cb17b38b137cf9b434bc3f920e
Beni Mahat <benimahat6565@gmail.com>
2020-09-25 23:42:21 -0600

    fix symbols

--- script.js
+++ script.js
@@ -11,25 +11,26 @@ var passworddisplay = document.getElementById('password');
 const uppercharcodes= charArray(65,90);
 const lowercharcodes= charArray(97,122);
 const numbercharcodes = charArray(48, 57);
-const symbolcharcodes = charArray(48, 57);
+const symbolcharcodes = charArray(33, 43);
 
-// When sclicking on the submit button hold the information
+// when the button is clicked store the values to the variable
 generateBtn.addEventListener('click',() =>{
   var length = numOfCharBtn.value;
   var upper = upperboxcheck.checked;
   var number = numberboxcheck.checked;
   var symbol = symbolboxcheck.checked;
-
+  //update markup to show password
   passworddisplay.innerText = generatePassword(length,upper,number,symbol);
 });
 
 function generatePassword(length,upper,number,symbol){
   let charCodes = lowercharcodes;
+  //If upper is true then add array to lowercharcodes array
   if (upper) charCodes = charCodes.concat(uppercharcodes);
   if (number) charCodes = charCodes.concat(numbercharcodes);
   if (symbol) charCodes = charCodes.concat(symbolcharcodes);
 
-  
+  //using our combined array with all our char codes uses a randomiizer to select and convert code to char
   const passwordChars = [];
   for (let i = 0; i < length; i++){ // index starting at zero, loop untill max characters met
       const arraynumber = charCodes[Math.floor(Math.random()* charCodes.length)]; // assigns random num from array
@@ -41,7 +42,7 @@ function generatePassword(length,upper,number,symbol){
 
 
 };
-
+//this creates an array of number between the given values
 function charArray(min,max){
   const array = [];
   for (let i = min; i<= max; i++) { // fill array with the given range
